b99f4772f9720af97d2e2a39a6722991ca3bce1f
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2024-04-09 19:56:22 -0600

    Updated Navbar

--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "public/android-chrome-192x192.png";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
 
 import { unstable_noStore as noStore } from "next/cache";
 import { getServerAuthSession } from "~/server/auth";
@@ -13,21 +14,34 @@ export default async function Navbar() {
     <nav className="sticky top-0 z-50 flex h-24 w-full flex-row items-center justify-between bg-slate-950 px-6 py-3 shadow-md">
       <Link
         href="/"
-        className="flex flex-row items-center justify-center gap-4"
+        className="flex flex-row items-center justify-center gap-4 hover:opacity-70"
       >
         <Image src={Logo} height={60} width={60} alt="Steam Deals Site Logo" />
         <h1 className="text-3xl text-cyan-300">Steam Deals</h1>
       </Link>
       <div className="my-7 flex flex-row items-center justify-center gap-4">
-        <p className="text-center text-2xl text-white">
-          {session && <span>{session.user?.name}</span>}
-        </p>
-        <Link
-          href={session ? "/api/auth/signout" : "/api/auth/signin"}
-          className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-        >
-          {session ? "Sign out" : "Sign in"}
-        </Link>
+        {session ? (
+          <div className="hover:cursor-pointer hover:opacity-70">
+            <p className="mr-2 inline text-center align-middle text-2xl text-white">
+              <span>{session.user?.name}</span>
+              <ChevronDownIcon className="ml-1 inline h-4 w-4" />
+            </p>
+            <div className="inline">
+              <img
+                src={`${session.user.image}`}
+                alt="Profile Photo"
+                className="inline  h-12 w-12 overflow-hidden rounded-full"
+              />
+            </div>
+          </div>
+        ) : (
+          <Link
+            href="/api/auth/signin"
+            className="rounded-full bg-cyan-300/90 px-6 py-2 font-semibold no-underline transition hover:bg-cyan-300/60"
+          >
+            Sign in
+          </Link>
+        )}
       </div>
     </nav>
   );
