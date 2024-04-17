fdaf1b589d5b7982dd50b9640ec65e9f399ceafe
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2024-04-17 15:14:57 -0600

    Removed Posts methods from router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { gameRouter } from "./routers/game";
 
@@ -8,7 +7,6 @@ import { gameRouter } from "./routers/game";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   games: gameRouter,
 });
 
