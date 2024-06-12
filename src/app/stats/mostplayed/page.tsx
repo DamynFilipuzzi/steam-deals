c4429f03b4cf3ce3c2bfd5caf4914532db1a03f3
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2024-06-12 16:40:27 -0600

    added various improvements to site accessibility

--- src/app/stats/mostplayed/page.tsx
+++ src/app/stats/mostplayed/page.tsx
@@ -5,7 +5,7 @@ export default async function Page() {
   const mostPlayed = await api.apps.mostPlayed.query();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-5 bg-black text-white">
-      <h2 className="pt-5 text-3xl">Most Played</h2>
+      <h1 className="pt-5 text-3xl">Most Played</h1>
       <div className="flex flex-col gap-5 pb-5 xl:flex-row">
         <MostPlayedTable games={mostPlayed} />
       </div>
