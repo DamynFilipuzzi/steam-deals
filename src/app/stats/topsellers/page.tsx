fb40966fb905af8958a917e7e97b91ed9e92db52
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2024-06-12 16:35:38 -0600

    added various improvements to site accessibility

--- src/app/stats/topsellers/page.tsx
+++ src/app/stats/topsellers/page.tsx
@@ -5,7 +5,7 @@ export default async function Page() {
   const topSelling = await api.apps.topSellers.query();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-5 bg-black text-white">
-      <h1 className="pt-5 text-3xl">Top Selling</h1>
+      <h2 className="pt-5 text-3xl">Top Selling</h2>
       <div className="flex flex-col gap-5 pb-5 xl:flex-row">
         <TopSellingTable games={topSelling} />
       </div>
