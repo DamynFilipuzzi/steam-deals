671763fbc4b7a9195e319c6b31ab9aa9d3da0969
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2024-06-14 01:52:22 -0600

    Updated image metadata

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -30,14 +30,12 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     `https://steamcdn-a.akamaihd.net/steam/apps/${game?.steam_id}/library_600x900.jpg`,
   );
   // if app has no library image use capsule image instead
-  let ogImageUrl = new URL("");
+  let ogImageUrl = new URL(
+    `https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/${game?.steam_id}/capsule_231x87.jpg`,
+  );
   if (capsuleFetch.ok) {
     ogImageUrl = new URL(
       `https://steamcdn-a.akamaihd.net/steam/apps/${game?.steam_id}/library_600x900.jpg`,
-    );
-  } else {
-    ogImageUrl = new URL(
-      `https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/${game?.steam_id}/capsule_231x87.jpg`,
     );
   }
 
