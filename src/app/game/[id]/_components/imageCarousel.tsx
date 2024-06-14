c825174dc720c586a5c70296bb937633225ed1ba
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2024-06-13 23:12:21 -0600

    updated autoscroll functionality

--- src/app/game/[id]/_components/imageCarousel.tsx
+++ src/app/game/[id]/_components/imageCarousel.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Autoplay from "embla-carousel-autoplay";
+import React from "react";
 import {
   Carousel,
   CarouselContent,
@@ -20,24 +21,22 @@ type props = {
 };
 
 export default function ImageCarousel({ screenshots }: props) {
+  const plugin = React.useRef(
+    Autoplay({ delay: 3000, stopOnInteraction: true }),
+  );
+
   return (
     <div>
       {screenshots.length > 0 && (
         <div className="mt-5 flex flex-col-reverse justify-center gap-4 px-5 xl:flex-row xl:px-80 ">
           <div className="h-full w-full bg-slate-900 p-5">
             <h2 className="mb-2 text-2xl text-cyan-500">Screenshots</h2>
-            <Carousel
-              plugins={[
-                Autoplay({
-                  delay: 2000,
-                }),
-              ]}
-            >
+            <Carousel plugins={[plugin.current]}>
               <CarouselContent>
                 {screenshots.map((image) => {
                   return (
                     <CarouselItem
-                      className="basis:1/3 md:basis-2/4"
+                      className="basis-5/6 md:basis-2/4"
                       key={image.id + "sid"}
                     >
                       <img
@@ -48,8 +47,8 @@ export default function ImageCarousel({ screenshots }: props) {
                   );
                 })}
               </CarouselContent>
-              <CarouselPrevious />
-              <CarouselNext />
+              <CarouselPrevious onMouseDown={plugin.current.stop} />
+              <CarouselNext onMouseDown={plugin.current.stop} />
             </Carousel>
           </div>
         </div>
