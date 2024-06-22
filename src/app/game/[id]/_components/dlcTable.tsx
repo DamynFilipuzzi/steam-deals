5c156d83e16d03c1373b9791a3660f0d2447e3c0
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2024-06-21 20:45:39 -0600

    feat: added pagination to dlc table

--- src/app/game/[id]/_components/dlcTable.tsx
+++ src/app/game/[id]/_components/dlcTable.tsx
@@ -10,6 +10,9 @@ import {
 } from "~/components/ui/table";
 import PriceDisplay from "~/app/_components/priceDisplay";
 import { useRouter } from "next/navigation";
+import { Button } from "~/components/ui/button";
+import { ArrowLeftIcon, ArrowRightIcon } from "lucide-react";
+import React from "react";
 
 type DLCProp = {
   appDLC: {
@@ -36,6 +39,34 @@ type DLCProp = {
 
 export default function DLCTable({ appDLC }: DLCProp) {
   const router = useRouter();
+  const [max, setMax] = React.useState(5);
+
+  const [prevBtnDisabled, setPrevButtonDisabled] = React.useState(true);
+  const [nextBtnDisabled, setNextButtonDisabled] = React.useState(false);
+
+  const showNext = () => {
+    if (max <= appDLC.length) {
+      setMax(max + 5);
+      if (prevBtnDisabled) {
+        setPrevButtonDisabled(false);
+      }
+      if (max + 5 >= appDLC.length) {
+        setNextButtonDisabled(true);
+      }
+    }
+  };
+
+  const showPrev = () => {
+    if (max - 5 > 0) {
+      setMax(max - 5);
+      if (nextBtnDisabled) {
+        setNextButtonDisabled(false);
+      }
+      if (max - 10 <= 0) {
+        setPrevButtonDisabled(true);
+      }
+    }
+  };
 
   return (
     <>
@@ -46,13 +77,13 @@ export default function DLCTable({ appDLC }: DLCProp) {
             <Table className="bg-background">
               <TableHeader>
                 <TableRow>
-                  <TableHead className="w-[140px]"></TableHead>
+                  <TableHead className=" w-[100px] lg:w-[140px]"></TableHead>
                   <TableHead className="text-left">Title</TableHead>
                   <TableHead className="text-right">Price</TableHead>
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {appDLC.map((dlc) => {
+                {appDLC.slice(max - 5, max).map((dlc) => {
                   return (
                     <TableRow
                       key={dlc.id}
@@ -77,6 +108,29 @@ export default function DLCTable({ appDLC }: DLCProp) {
                 })}
               </TableBody>
             </Table>
+            {appDLC.length > 5 && (
+              <div className="mt-5 flex w-full flex-row items-center justify-between">
+                <span className="inline">
+                  {max > appDLC.length ? appDLC.length : max} of {appDLC.length}
+                </span>
+                <div>
+                  <Button
+                    disabled={prevBtnDisabled}
+                    variant="outline"
+                    onClick={() => showPrev()}
+                  >
+                    <ArrowLeftIcon />
+                  </Button>
+                  <Button
+                    disabled={nextBtnDisabled}
+                    variant="outline"
+                    onClick={() => showNext()}
+                  >
+                    <ArrowRightIcon />
+                  </Button>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       )}
