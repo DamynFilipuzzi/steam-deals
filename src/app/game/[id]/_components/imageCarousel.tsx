deb83eca889664187236735d9ade5cb46f7a67ed
Damyn Filipuzzi <damynfilipuzzi@gmail.com>
2024-08-01 17:17:27 -0600

    update: style and interaction for app video

--- src/app/game/[id]/_components/imageCarousel.tsx
+++ src/app/game/[id]/_components/imageCarousel.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { PlayCircle } from "lucide-react";
 import Autoplay from "embla-carousel-autoplay";
 import React from "react";
 import {
@@ -12,8 +13,10 @@ import {
 
 import {
   Dialog,
-  DialogContentCustomClose,
+  DialogContent,
   DialogDescription,
+  DialogHeader,
+  DialogTitle,
   DialogTrigger,
 } from "~/components/ui/dialog";
 
@@ -56,16 +59,43 @@ export default function ImageCarousel({ screenshots, videos }: props) {
                       key={video.video_id}
                       className="basis-5/6 md:basis-2/4 lg:basis-1/3 2xl:basis-1/4 3xl:basis-1/3 4xl:basis-1/6 5xl:basis-1/5"
                     >
-                      <video
-                        controls
-                        poster={`https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/${video.video_id}/movie.293x165.jpg?t=1721739680`}
-                      >
-                        <source
-                          src={`http://cdn.akamai.steamstatic.com/steam/apps/${video.video_id}/movie480.mp4?t=1696005467`}
-                          type="video/mp4"
-                          title={video.video_name}
-                        />
-                      </video>
+                      <Dialog>
+                        <DialogTrigger
+                          onMouseDown={plugin.current.stop}
+                          className="h-full w-full"
+                        >
+                          <div className="group relative">
+                            <img
+                              className="m-auto h-full w-full"
+                              src={`https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/${video.video_id}/movie.293x165.jpg`}
+                              alt={video.video_name}
+                            />
+                            <PlayCircle
+                              className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 transform rounded-lg bg-black/70 p-1 text-slate-300 group-hover:text-slate-500"
+                              size={38}
+                            />
+                          </div>
+                        </DialogTrigger>
+                        <DialogContent className="max-w-7xl border-0 bg-background">
+                          <DialogHeader>
+                            <DialogTitle></DialogTitle>
+                            <DialogDescription>
+                              <video
+                                className="m-auto"
+                                controls
+                                poster={`https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/${video.video_id}/movie.293x165.jpg`}
+                              >
+                                {/* http://cdn.akamai.steamstatic.com/steam/apps/${video.video_id}/movie_max.mp4 */}
+                                <source
+                                  src={`http://cdn.akamai.steamstatic.com/steam/apps/${video.video_id}/movie480.mp4`}
+                                  type="video/mp4"
+                                  title={video.video_name}
+                                />
+                              </video>
+                            </DialogDescription>
+                          </DialogHeader>
+                        </DialogContent>
+                      </Dialog>
                     </CarouselItem>
                   );
                 })}
@@ -82,15 +112,18 @@ export default function ImageCarousel({ screenshots, videos }: props) {
                             alt={`Image: ${image.id}`}
                           />
                         </DialogTrigger>
-                        <DialogContentCustomClose className="max-w-7xl border-0 bg-transparent">
-                          <DialogDescription>
-                            <img
-                              className="m-auto"
-                              src={image.path_full}
-                              alt={`Image: ${image.id}`}
-                            />
-                          </DialogDescription>
-                        </DialogContentCustomClose>
+                        <DialogContent className="max-w-7xl border-0 bg-background">
+                          <DialogHeader>
+                            <DialogTitle></DialogTitle>
+                            <DialogDescription>
+                              <img
+                                className="m-auto"
+                                src={image.path_full}
+                                alt={`Image: ${image.id}`}
+                              />
+                            </DialogDescription>
+                          </DialogHeader>
+                        </DialogContent>
                       </Dialog>
                     </CarouselItem>
                   );
